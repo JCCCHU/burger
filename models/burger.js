0b15510ad5e69eec46d72180482302caa4ec2f5f
Joshua Chu <jashya@gmail.com>
2020-07-11 22:59:48 -0400

    Add burger model

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,21 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  selectAll: function()
+  all: function() {
+    orm.all(function (res) {
+      return res;
+    });
+  },
+  create: function(name) {
+    orm.insertOne(name, function(res) {
+      return res;
+    });
+  },
+  update: function(name) {
+    orm.updateOne(name, function(res) {
+      return res;
+    });
+  }
 }
+
+module.exports = burger;
